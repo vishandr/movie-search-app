59bd3e240953b1592ae0c32740a0e02aa7ec5400
vishandr <101496759+vishandr@users.noreply.github.com>
2024-10-24 10:14:13 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import SearchPanel from './SearchPanel.tsx';
+function App() {
+  return (
+    <>
+      <div className='bg-sky-300'>
+        <SearchPanel />
+      </div>
+    </>
+  );
+}
+
+export default App;
